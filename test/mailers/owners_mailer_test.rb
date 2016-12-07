7d2413f8816cf90d3287ce40796d5b75278b8589
Russell Osborne <russell@burningpony.com>
2016-12-07 10:24:29 -0500

    Fix owner mailer expect from address

--- test/mailers/owners_mailer_test.rb
+++ test/mailers/owners_mailer_test.rb
@@ -5,7 +5,7 @@ class OwnersMailerTest < ActionMailer::TestCase
     mail = OwnersMailer.appointment
     assert_equal "Appointment", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["russell@theironyard.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
