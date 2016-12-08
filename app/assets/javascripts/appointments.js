07304dfc79c112715a3a454663693be4f314382d
Russell Osborne <russell@burningpony.com>
2016-12-08 09:46:33 -0500

    Added reload and autorefresh functionality to the welcome page

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,21 +1,30 @@
 console.log("Hello World");
 
-$.ajax("/appointments/current", {
-  contentType: "application/json",
-  dataType: "json"
-}).done(function appointmentsLoaded(appointments) {
-  appointments.forEach(function getPet(appointment) {
+function loadAppointments() {
+  $.ajax("/appointments/current", {
+    contentType: "application/json",
+    dataType: "json"
+  }).done(function appointmentsLoaded(appointments) {
+    $(".active-appointments").html("");
 
-    $.ajax("/pets/" + appointment.pet_id, {
-      contentType: "application/json",
-      dataType: "json"
-    }).done(function (pet) {
-      console.log(pet, appointment);
-      appointment.pet = pet;
-      renderAppointment(appointment);
+    appointments.forEach(function getPet(appointment) {
+
+      $.ajax("/pets/" + appointment.pet_id, {
+        contentType: "application/json",
+        dataType: "json"
+      }).done(function (pet) {
+        console.log(pet, appointment);
+        appointment.pet = pet;
+        renderAppointment(appointment);
+      });
     });
   });
-});
+}
+
+loadAppointments();
+
+$("#reload-appointments").on("click", loadAppointments);
+window.setInterval(loadAppointments, 5 * 1000);
 
 function findPetById(id) {
 
@@ -24,7 +33,7 @@ function findPetById(id) {
 
 function renderAppointment (appointment) {
   $(".active-appointments").append(
-   `<p>${appointment.pet.name}</p>`
+   "<p>" + appointment.pet.name  + "</p>"
   );
 }
 
